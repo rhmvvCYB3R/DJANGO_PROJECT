382971fd4c9f0cb8192f7e0197196724b20a195a
rhmvvCYB3R <10ragimov10@gmail.com>
2025-02-23 01:37:15 +0100

    UPD

--- cybersec/base/views.py
+++ cybersec/base/views.py
@@ -2,10 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return render (request, 'home.html')
+rooms = [
+    {'id':1, 'name': 'Lets learn python!'},
+    {'id':2, 'name': 'Hack with me'},
+    {'id':3, 'name': 'Reverse Engineers'},
+]
 
-def room(request):
-    return render(request, 'room.html')
+
+
+def home(request):
+    context = {'rooms': rooms}
+    return render (request, 'base/home.html', context )
+
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room =i 
+    context = {'room': room}
+    return render(request, 'base/room.html',context)
 
 
